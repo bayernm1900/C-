0998bc2aaaf982cc77d607599933c0df2eeb0a5c
Dimon <dmitriykorzhov25msm@gmail.com>
2023-02-11 22:46:37 +0300

    Добавили задачу номер 4

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int y = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int z = int.Parse(Console.ReadLine());
+int max = x;
+if (x > y) max = x;
+if (x > z) max = x;
+if (y > x) max = y;
+if (y > z) max = y;
+if (z > x) max = z;
+if (z > y) max = z;
+{
+    System.Console.WriteLine(max);
+}
