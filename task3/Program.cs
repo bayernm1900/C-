db257ab92b610e53bfb058fe3c8922d20358fa4e
Dimon <dmitriykorzhov25msm@gmail.com>
2023-02-11 23:00:58 +0300

    Добавили задачу номер 3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int numbers = int.Parse(Console.ReadLine());
+if (numbers % 2 == 0)
+{
+    System.Console.WriteLine("Число является четным");
+}
+else 
+{
+    System.Console.WriteLine("Число является нечетным");
+}
