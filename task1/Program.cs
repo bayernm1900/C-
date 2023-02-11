8ee16e31c1b01b342f787cf57e7b3e00ffb3567e
Dimon <dmitriykorzhov25msm@gmail.com>
2023-02-11 22:17:11 +0300

    Домашнее задание Задача 2

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите второе чиcло: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > y) 
+{
+    System.Console.WriteLine("Первое число больше второго");
+}
+if ( y > x )
+{
+    System.Console.WriteLine("Второе число больше первого");
+}
+
