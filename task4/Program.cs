48ca550b1d5d1152bf9eacf10ef708d4a85109d6
Dimon <dmitriykorzhov25msm@gmail.com>
2023-02-12 00:05:10 +0300

    Добавили попытки решения 4 задачи

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число от 1....100 ");
+int N = int.Parse(Console.ReadLine());
+int count = 0;
+int T = (N);
+for (count = 0; count < N; count = count + 1);
+if (N % 2 == 0)
+{
+    System.Console.WriteLine(T);
+}
+
