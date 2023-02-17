63999c5eac59121eb09c1907d46c16579c92afb4
Dimon <dmitriykorzhov25msm@gmail.com>
2023-02-17 15:16:35 +0300

    Save task5

---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначиное число ");
+ int numbers = int.Parse(Console.ReadLine());
+ Console.WriteLine(numbers);
+
+ int a = numbers / 100;
+ int b = numbers % 10;
+ int c = numbers / 10 % 10;
+ Console.WriteLine(c);
