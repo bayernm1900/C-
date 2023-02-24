131c5e9b9aaa178cf616028c10aa001d7ed0d246
Dimon <dmitriykorzhov25msm@gmail.com>
2023-02-24 18:03:05 +0300

    Добавили решение таск 6

---
+++ task6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+if (N < 100)
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
+else
+{
+while ( N >= 1000)
+{
+    N = N /10;
+}
+int thirdNumner = N % 10;
+Console.WriteLine($"Третья цифра: { thirdNumner }");
+
+}
